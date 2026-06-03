550152b63b1b978e2d2c6e282fbdfb63fa252c4c
davidggjg <david.batish1@gmail.com>
2026-06-03 07:41:36 +0300

    Add work-runtime-ktx dependency to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,4 +50,5 @@ dependencies {
     implementation(libs.material)
     implementation("androidx.security:security-crypto:1.1.0-alpha06")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("androidx.work:work-runtime-ktx:2.9.0")
 }
